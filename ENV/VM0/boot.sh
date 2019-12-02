073c1be002203b69678ded62fc777eac58c199d1
Johan Louwers <louwersj@gmail.com>
2019-12-02 13:17:40 +0100

    Update boot.sh
    Minor cosmetic changes to log text.

--- ENV/VM0/boot.sh
+++ ENV/VM0/boot.sh
@@ -8,19 +8,19 @@ CONF_GIT_BASE="https://raw.githubusercontent.com/louwersj/OCI_DEMO_U7/master/ENV
 sudo cd /tmp
 
 # Get the fedora EPEL repo and install it. This is required to able to install ansible on the node. 
-logger "boot.sh - download EPEL RPM"
+logger "boot.sh - Download EPEL RPM"
 sudo curl http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --output /tmp/epel.rpm --silent
 sudo rpm -Uvh /tmp/epel.rpm
 
 # Install ansible for node configuration. We will be using ansible for all the configuration options. 
-logger "boot.sh - install ansible"
+logger "boot.sh - Install ansible"
 sudo yum -y install ansible
 
 # get the default ansible playbook. The default ansible playbook for each node is named 
 # "node_configuration.yaml and use CONF_GIT_BASE to form the full URI.
-logger "boot.sh - download node_configuration from $CONF_GIT_BASE/node_configuration.yaml"
+logger "boot.sh - Download node_configuration from $CONF_GIT_BASE/node_configuration.yaml"
 sudo curl "$CONF_GIT_BASE/node_configuration.yaml" --output /tmp/node_configuration.yaml
 
 # Run the default ansible playbook.
-logger "boot.sh - run ansible playbook /tmp/node_confirguration.yaml"
+logger "boot.sh - Run ansible playbook /tmp/node_confirguration.yaml"
 sudo ansible-playbook /tmp/node_configuration.yaml
