df7fb8b0bb4dd708b4b4b639813261be9279af10
Johan Louwers <louwersj@gmail.com>
2019-12-02 13:09:19 +0100

    Update boot.sh

--- ENV/VM0/boot.sh
+++ ENV/VM0/boot.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# Ensure this variable points to the GIT location where we store the config for this specific node. 
+CONF_GIT_BASE="https://raw.githubusercontent.com/louwersj/OCI_DEMO_U7/master/ENV/VM0/"
+
+# Ensure we are fully fully certain that we run in /tmp even though we explicitly state /tmp in the 
+# used commands we do not want to take any change that we dirty our newly created VM
 sudo cd /tmp
 
-# Get the fedora EPEL repo and install it. This is required to 
-# able to install ansible on the node. 
+# Get the fedora EPEL repo and install it. This is required to able to install ansible on the node. 
 sudo curl http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --output /tmp/epel.rpm --silent
 sudo rpm -Uvh /tmp/epel.rpm
 
-# Install ansible for node configuration. We will be using ansible 
-# for all the configuration options. 
+# Install ansible for node configuration. We will be using ansible for all the configuration options. 
 sudo yum -y install ansible
 
-# get the default ansible playbook. The default ansible playbook
-# for each node is named "node_configuration.yaml
-sudo curl https://raw.githubusercontent.com/louwersj/OCI_DEMO_U7/master/ENV/VM0/node_configuration.yaml --output /tmp/node_configuration.yaml --silent
+# get the default ansible playbook. The default ansible playbook for each node is named 
+# "node_configuration.yaml and use CONF_GIT_BASE to form the full URI.
+sudo curl "$CONF_GIT_BASE/node_configuration.yaml" --output /tmp/node_configuration.yaml
 
 # Run the default ansible playbook.
 sudo ansible-playbook /tmp/node_configuration.yaml
