6e2d72f7594766fb364a501142536067f204267e
Johan Louwers <louwersj@gmail.com>
2019-12-02 13:33:27 +0100

    Create profile.sh

---
+++ ENV/INFRA/terraform/profile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export TF_VAR_tenancy_ocid=PROVIDETenancyOcId
+export TF_VAR_user_ocid=PROVIDEUserOcId
+export TF_VAR_fingerprint=PROVIDEFingerprint
+export TF_VAR_region=PROVIDERegion
+export TF_VAR_private_key_path=PROVIDEPrivateKeyPath
+export TF_VAR_private_key_password=PROVIDEPrivateKeyPassword
+export TF_VAR_compartment_ocid=PROVIDECompartmentOCID
